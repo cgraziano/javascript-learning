5baa8fe1d34a922b238a27628a31b901082edcb9
cgraziano <cgraziano9363@gmail.com>
2019-05-22 22:30:24 -0600

    Variable name lesson

---
+++ hello-world/script.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+//variable name
+let productName = "Hammer";
+let productId = "H123";
+
+//Also
+//let productName = "Hammer",
+//	productId = "H123";
+
+//single vs double quote
+productName = "Hammer's";
+console.log(productName);
